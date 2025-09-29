f92a72783a0bbf78402d5229754ca1a0ba02e80e
Ole Kodehode <ole.kodehode@gmail.com>
2025-09-29 14:55:23 +0200

    title class change

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import NavButton from "./NavButton";
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full bg-zinc-100 text-zinc-900 dark:bg-zinc-800/50 dark:text-zinc-200 h-18 flex p-5 justify-between">
-      <h5 className="text-3xl">Kode-Ole</h5>
+      <h5 className="text-3xl font-roboto">Kode-Ole</h5>
       <ul className="flex gap-5 text-2xl">
         <li>
           <NavButton to="/" />
