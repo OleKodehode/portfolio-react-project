dafc580e587b8eb7405c2d23c4ae3a60a9f04468
Ole Kodehode <ole.kodehode@gmail.com>
2025-09-29 14:50:34 +0200

    class changes

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -7,7 +7,7 @@ interface NavButtonProps {
 export default function NavButton({ to }: NavButtonProps) {
   return (
     <Link to={to}>
-      <button className="p-2 -translate-y-2 text-white bg-purple-500 hover:bg-purple-700 shadow-lg outline outline-zinc-300/5 dark:bg-purple-700 dark:hover:bg-purple-500 rounded-xl hover:underline hover:-translate-y-2.5 dark:shadow-none dark:outline-none cursor-pointer">
+      <button className="p-2 -translate-y-2 text-white bg-purple-500 hover:bg-purple-700 drop-shadow-lg outline outline-zinc-300/5 dark:bg-purple-700 dark:hover:bg-purple-500 rounded-xl hover:underline hover:-translate-y-2.5 dark:shadow-none dark:outline-none cursor-pointer">
         {to === "/" ? "Home" : to[0].toUpperCase() + to.slice(1)}{" "}
       </button>
     </Link>
